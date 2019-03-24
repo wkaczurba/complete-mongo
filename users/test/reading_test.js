e22c286b8b3622bf522f4791917792f4212471bf
wkaczurba <witold.kaczurba@gmail.com>
2019-03-24 17:14:22 +0000

    Delete tests

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -8,7 +8,7 @@ describe('Reading users out of the database', () => {
         joe = new User({name:'Joe'});
         joe.save()
             .then((savedUser) => {
-                console.log('saved!: ' + savedUser)
+                //console.log('saved!: ' + savedUser)
                 done();
             });
         
@@ -17,11 +17,18 @@ describe('Reading users out of the database', () => {
     it('finding user', (done) => {
         User.find( { name: 'Joe' })
             .then( (users) => {
-                    console.log('users[0]: ' + users[0]);
+                    //console.log('users[0]: ' + users[0]);
                     assert(users[0]._id.toString() === joe._id.toString())
                     done();
                 }
             );
     });
 
+    it('finding a user with a particular id', (done) => {
+        User.findOne( { _id:joe._id })
+            .then((user) => {
+                assert(user.name === 'Joe')
+                done();
+            });
+    })
 })

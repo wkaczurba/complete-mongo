06bb7c2452e2bbb1beddc0414d0e5cebc52d736d
wkaczurba <witold.kaczurba@gmail.com>
2019-03-26 07:25:01 +0000

    Handling Big Collections with Pagination

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,15 +2,21 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Reading users out of the database', () => {
-    let joe;
+    let joe, maria, alex, zach;
 
     beforeEach( (done) => {
-        joe = new User({name:'Joe'});
-        joe.save()
-            .then((savedUser) => {
-                //console.log('saved!: ' + savedUser)
-                done();
-            });
+        joe = new User({ name: 'Joe' });
+        maria = new User({ name: 'Maria' });
+        alex = new User({ name: 'Alex' });
+        zach = new User({ name: 'Zach' });
+    
+        //joe.save()
+        //	.then( () => done());
+        // NOTE: THis Promise does not guarantee in DB. 
+        // It will execute consecutively 
+         // but Mongo may save them in different order (e.g internal delays)
+        Promise.all( [ joe.save(), maria.save(), alex.save(), zach.save() ])
+            .then( () => done());
         
     });
 
@@ -32,5 +38,17 @@ describe('Reading users out of the database', () => {
             });
     })
 
-    
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort( { name: 1 } ) 	// sort all users by name property: Alex, Joe, Maria, Zachs
+            .skip(1)
+            .limit(2)
+            .then((users) => { 
+                assert(users.length === 2);
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
+    });
+        
 })

814693721d32527258b0a7461e953c8d19943bdb
wkaczurba <witold.kaczurba@gmail.com>
2019-03-27 07:15:36 +0000

    upstarmusic: update retired field

--- upstarmusic/database/queries/SetNotRetired.js
+++ upstarmusic/database/queries/SetNotRetired.js
@@ -6,4 +6,7 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves after the update
  */
 module.exports = (_ids) => {
+    return Artist.updateMany(
+        { _id : { $in : _ids }},
+        { retired : false } );
 };

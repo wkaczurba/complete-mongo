53762fa4ede369e99ed056000be33140f0f385b7
wkaczurba <witold.kaczurba@gmail.com>
2019-03-27 00:18:32 +0000

    upstartmusic: schema + basic queries

--- upstarmusic/database/models/album.js
+++ upstarmusic/database/models/album.js
@@ -1,1 +1,15 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    title: String,
+    date: String,
+    copiesSold: Number,
+    numberTracks: Number,
+    image: String,
+    revenue: Number
+})
+
+const Album = mongoose.model('album', AlbumSchema);
+module.export = Album
+
--- upstarmusic/database/models/artist.js
+++ upstarmusic/database/models/artist.js
@@ -1,1 +1,19 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Album = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    webiste: String,
+    netWorth: String,
+    labelName: String,
+    retired: Boolean,
+    albums: [Album]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+module.exports = Artist;

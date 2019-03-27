46ad27699bd455c618cde6f36c8e894059cca3cc
wkaczurba <witold.kaczurba@gmail.com>
2019-03-27 01:31:45 +0000

    upstartmusic: searching 1-tricky

--- upstarmusic/database/queries/SearchArtists.js
+++ upstarmusic/database/queries/SearchArtists.js
@@ -9,4 +9,27 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+
+    // Non-ES6 approach:
+    //let sortArray = {};
+    //sortArray[sortProperty] = 1;
+
+    //ES6 approach
+    let sortArray = { [sortProperty] : 1 };
+
+    const query = Artist.find()
+         .sort(sortArray)
+         .skip(offset)
+         .limit(limit);
+
+    // the return prokmise should look like:
+    // { all: [artists], count: count, offset : offset, limit: limit }
+    return Promise.all( [ query, Artist.count() ])
+        .then( (result) => {
+            return { all: result[0],
+                count: result[1],
+                offset: offset,
+                limit: limit };
+            });
 };
+

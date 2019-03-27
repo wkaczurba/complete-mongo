9090e6e3fb2f25e250b9644ad7c943f71eba7515
wkaczurba <witold.kaczurba@gmail.com>
2019-03-27 00:58:04 +0000

    upstartmusic: min/max queries

--- upstarmusic/database/queries/GetAgeRange.js
+++ upstarmusic/database/queries/GetAgeRange.js
@@ -6,4 +6,23 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minAgeQuery = Artist
+        .find()
+        .sort( { age : 1 })
+        .limit(1)
+        .then( artist => artist[0].age);
+
+    const maxAgeQuery = Artist
+        .find()
+        .sort( { age : -1 })
+        .limit(1)
+        .then( artist => artist[0].age);
+
+    return Promise.all([minAgeQuery, maxAgeQuery])
+        .then((result)  => {
+        return {
+            min: result[0],
+            max: result[1]
+        }});
 };
+
--- upstarmusic/database/queries/GetYearsActiveRange.js
+++ upstarmusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+
+    const minYearsActiveRangeQuery = Artist
+        .find()
+        .sort( { yearsActive : 1 })
+        .limit(1)
+        .then( artists => artists[0].yearsActive);
+
+    const maxYearsActiveRangeQuery = Artist
+        .find()
+        .sort( { yearsActive : -1})
+        .limit(1)
+        .then( artists => artists[0].yearsActive);
+
+    return Promise.all([ minYearsActiveRangeQuery, maxYearsActiveRangeQuery ])
+        .then( (result) => {
+            return { min: result[0], max: result[1] }
+        } );
 };
+
